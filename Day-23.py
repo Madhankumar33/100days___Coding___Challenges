3e90e01749a3936f9e87a1c3e5bcf41f85802175
Madhankumar33 <138284816+Madhankumar33@users.noreply.github.com>
2023-07-16 14:22:59 +0530

    Add files via upload

---
+++ Day-23.py
@@ -0,0 +1,7 @@
+# cook your dish here
+# cook your code here
+T=int(input())
+for i in range(T):
+    X=list(map(int,input().split()))
+    X=sorted(X)
+    print(X[1])
---
+++ Day-24.py
@@ -0,0 +1,10 @@
+# cook your code here
+T=int(input())
+for i in range(T):
+    X=int(input())
+    if(X>30):
+        print("YES")
+    else:
+        print("NO")
+        
+    
