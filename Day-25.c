bc4202451b9705d9243b1298e0ac4416744c6af0
Madhankumar33 <138284816+Madhankumar33@users.noreply.github.com>
2023-07-18 09:26:27 +0530

    Add files via upload

---
+++ Day-25.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    int i,j,n;
+    printf("Enter Number of rows ");
+    scanf("%d",&n);
+    for(i=n;i>=1;i--) {
+            printf("* * * * *");
+        
+        printf("\n");
+    }
+
+    return 0;
+}
